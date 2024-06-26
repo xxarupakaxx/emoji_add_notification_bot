f9f6399398c19b3d09d34b82bf73b84aefac5aa0
yoshiki-morii <yoshiki.morii@dena.com>
2024-06-27 00:41:50 +0900

    reactionHandler

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.22.3
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/slack-go/slack v0.13.0 // indirect
 )
--- handler/reaction.go
+++ handler/reaction.go
@@ -1,8 +1,15 @@
 package handler
 
 import (
+	"fmt"
+	"image"
+	"image/png"
 	"log"
+	"net/http"
+	"os"
+	"strings"
 
+	"github.com/nfnt/resize"
 	"github.com/slack-go/slack"
 	"github.com/xxarupakaxx/emoji_add_notification_bot/config"
 )
@@ -10,11 +17,69 @@ import (
 func HandleReaction(evt *slack.ReactionAddedEvent, client *slack.Client) error {
 	if evt.Reaction != "done" {
 		log.Println("reaction is not done")
+		return fmt.Errorf("reaction is not done")
+	}
+
+	if config.GetConfig().AdminUserID == "" {
+		log.Println("admin user id is not set")
+		return fmt.Errorf("admin user id is not set")
+	}
+
+	msgInfo := slack.NewRefToMessage(evt.Item.Channel, evt.Item.Timestamp)
+	history, err := client.GetConversationHistory(&slack.GetConversationHistoryParameters{
+		ChannelID: evt.Item.Channel,
+		Latest:    evt.Item.Timestamp,
+		Limit:     1,
+		Inclusive: true,
+	})
+	if err != nil {
+		log.Println("failed to get conversation history", err)
+		return fmt.Errorf("failed to get conversation history: %w", err)
+	}
+
+	if len(history.Messages) == 0 || len(history.Messages[0].Files) == 0 {
+		log.Println("no message found OR no file found")
 		return nil
 	}
 
-	if config.Config.AdminUserID == "" {
-		log.Println("admin user id is not set")
-		return nil
+	file := history.Messages[0].Files[0]
+
+	resp, err := http.Get(file.URLPrivateDownload)
+	if err != nil {
+		log.Println("failed to download file", err)
+		return fmt.Errorf("failed to download file: %w", err)
 	}
+	defer resp.Body.Close()
+
+	img, _, err := image.Decode(resp.Body)
+	if err != nil {
+		log.Println("failed to decode image", err)
+		return fmt.Errorf("failed to decode image: %w", err)
+	}
+
+	resizedImage := resize.Resize(128, 128, img, resize.Lanczos3)
+
+	emojiName := fmt.Sprintf("%s", file.Name[:len(file.Name)-len(file.Filetype)-1])
+	emojiName = strings.ReplaceAll(emojiName, " ", "")
+
+	tempFile, err := os.CreateTemp("", "emoji_*.png")
+	if err != nil {
+		log.Println("failed to create temp file", err)
+		return fmt.Errorf("failed to create temp file: %w", err)
+	}
+
+	defer os.Remove(tempFile.Name())
+
+	png.Encode(tempFile, resizedImage)
+	tempFile.Close()
+	if err := addEmoji(emojiName, tempFile.Name()); err != nil {
+		log.Println("failed to add emoji", err)
+		return fmt.Errorf("failed to add emoji: %w", err)
+	}
+
+	return nil
+}
+
+func addEmoji(name, path string) error {
+	return nil
 }
